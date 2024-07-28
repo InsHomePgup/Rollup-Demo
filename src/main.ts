3a15b2af232c5303509e70bbfe12770247c82a35
InsHomePgup <7328234@gmail.com>
2024-07-28 15:36:25 +0800

    Import Value can not change in outside

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import * as Demo from './demo'  // namespace imports (Import everything from the source module as an object which exposes all the source module's named exports as properties and methods.)
-import {testName} from "./test";  // named imports (Import a specific item from a source module, with its original name.)
+import * as Demo from './nameSpace.ts'  // namespace imports (Import everything from the source module as an object which exposes all the source module's named exports as properties and methods.)
+import {testName} from "./named.ts";  // named imports (Import a specific item from a source module, with its original name.)
 import {version} from './version'
 import comm from './comm'  // default imports  (Import the default export of the source module.)
 /**
@@ -16,6 +16,17 @@ comm.comm.abc();
 import('./Dynamic.ts').then((module)=>{
     console.log("export module",module)
 })
+
+/**
+ * Is import value can change?
+ */
+
+import {count,increment} from "./change.ts";
+console.log(count)
+increment();
+console.log(count)
+// count++;  // It was error  : TS2632: Cannot assign to count because it is an import.
+
 
 // @ts-ignore
 console.log(Demo.myName,version,testName,window.abc)
